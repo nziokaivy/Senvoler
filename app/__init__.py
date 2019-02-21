1067f1cb5b6d478fd942beb08f730058345d437a
joy <wendokingoo@gmail.com>
2019-02-21 15:54:44 +0300

    added authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-#from config import config_options
+from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
@@ -22,7 +22,7 @@ def create_app(config_name):
     app = Flask(__name__)
 
     # Creating the app configurations
-    #app.config.from_object(config_options[config_name])
+    app.config.from_object(config_options[config_name])
 
     # Initializing flask extensions
     bootstrap.init_app(app)
--- config.py
+++ config.py
@@ -4,20 +4,15 @@ class Config:
     '''
     General configuration parent class
     '''
-<<<<<<< HEAD
-   
-    API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
-    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://AbbyShabi:dammy@localhost/iblog'
-=======
-    FLIGHT_API_BASE_URL ='https://dev-sandbox-api.airhob.com/sandboxapi/activities/v1/search{}?api_key={}''
+    FLIGHT_API_BASE_URL ='https://dev-sandbox-api.airhob.com/sandboxapi/activities/v1/search{}?api_key={}'
     FLIGHT_API_KEY = os.environ.get('FLIGHT_API_KEY')
->>>>>>> aaf072ed9e45d0feb4f0bf9cb79421631549f977
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
