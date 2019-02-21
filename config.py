793c3756868f94d66843b4d771f56e825de9eaa2
nziokaivy <nziokaivy@gmail.com>
2019-02-21 16:36:25 +0300

    allow new features

--- config.py
+++ config.py
@@ -4,14 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
-<<<<<<< HEAD
-   
-    API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
-    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://AbbyShabi:dammy@localhost/iblog'
-    FLIGHT_API_BASE_URL ='https://dev-sandbox-api.airhob.com/sandboxapi/activities/v1/search{}?api_key={}''
-=======
     FLIGHT_API_BASE_URL ='https://dev-sandbox-api.airhob.com/sandboxapi/activities/v1/search{}?api_key={}'
->>>>>>> 1067f1cb5b6d478fd942beb08f730058345d437a
     FLIGHT_API_KEY = os.environ.get('FLIGHT_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
