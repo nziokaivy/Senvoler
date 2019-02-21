598a0a1196cf278913d02792362cf6ba846dcdab
abby-shabi <abbyshabi@gmail.com>
2019-02-21 10:37:38 +0300

    styled the landing page

---
+++ app/main/views.py
@@ -0,0 +1,17 @@
+from flask import render_template,request,redirect,url_for,abort,flash
+from . import main
+from flask_login import login_required,current_user
+import datetime
+
+
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+ 
+    title = 'Home - Welcome to The best Movie Review Website Online'
+  
+    
+    return render_template('index.html')
